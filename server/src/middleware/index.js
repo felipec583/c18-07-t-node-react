ba1ab43e03f4e304d1c4dec4bacd12817395cf84
pablo lopez <pablo_98_18@hotmail.com>
2024-05-23 22:29:29 -0300

    added credentials to verifyToken.js

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,7 +1,11 @@
 import hashPassword from "./hashPassword.js";
+import checkUserId from "./checkUserId.js";
+import verifyToken from "./verifyToken.js";
 
 const middlewares = {
 	hashPassword,
+	checkUserId,
+	verifyToken
 }
 
 export default middlewares
