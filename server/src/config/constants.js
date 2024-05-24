5b1a93549ed30feadb6549fc8aa2915b6ade1ebe
Felipe C <felipecastillo48@gmail.com>
2024-05-24 12:09:07 -0400

    chore: current date constant

--- server/src/config/constants.js
+++ server/src/config/constants.js
@@ -1,7 +1,12 @@
 import "dotenv/config.js";
 
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI
-const JWT_SECRET = process.env.JWT_SECRET
+const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export { PORT, MONGO_URI, JWT_SECRET };
+let date = new Date();
+const CURRENT_DATE = new Date(
+  date.getTime() - date.getTimezoneOffset() * 60000
+).toISOString();
+
+export { PORT, MONGO_URI, JWT_SECRET, CURRENT_DATE };
