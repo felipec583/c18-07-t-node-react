947997bca565cc410cf53bb83fda47a9c198ca3a
pablo lopez <pablo_98_18@hotmail.com>
2024-05-26 17:21:37 -0300

    added /api/user/list endpoints and /api/auth/google

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import userService from "../services/user.service.js";
 const addBookToUserLibrary = async (req, res, next) => {
   try {
     const { id } = req.credentials;
-    const { bookId, userId } = req.body;
+    const { bookId } = req.body;
     const addBookToLibrary = await userService.addBookToUserLibrary(
       bookId,
       id
@@ -43,9 +43,62 @@ const createUserList = async (req, res, next) => {
     const { id } = req.credentials;
     const { listname } = req.body;
     const response = await userService.createUserList(id, listname)
-    if (response) {
-      return res.status(201).json({ success: true, response })
-    }
+    return res.status(201).json({ success: true, response })
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+const addBookToUserList = async (req, res, next) => {
+  try {
+    const { listId, bookId } = req.params
+    const { id } = req.credentials
+    const response = await userService.addBookToUserList(id, listId, bookId)
+    return res.status(200).json({ success: true, response })
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+const deleteUserListFromListId = async (req, res, next) => {
+  try {
+    const { listId } = req.params
+    const { id } = req.credentials
+    const response = await userService.deleteUserListFromListId(id, listId)
+    return res.status(200).json({ success: true, response })
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+const deleteBookFromListId = async (req, res, next) => {
+  try {
+    const { listId, bookId } = req.params
+    const { id } = req.credentials
+    const response = await userService.deleteBookFromListId(id, listId, bookId)
+    return res.status(200).json({ success: true, response })
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+const changeListNameFromListId = async (req, res, next) => {
+  try {
+    const { listId, newName } = req.params
+    const { id } = req.credentials
+    const response = await userService.changeListNameFromListId(id, listId, newName)
+    return res.status(200).json({ success: true, response })
+  } catch (err) {
+    return res.status(500).json({ success: false, message: err.message })
+  }
+}
+
+const getUserList = async (req, res, next) => {
+  try {
+    const { userId } = req.params
+    const { listId } = req.query
+    const response = await userService.getUserList(userId, listId)
+    return res.status(200).json({ success: true, response })
   } catch (err) {
     return res.status(500).json({ success: false, message: err.message })
   }
@@ -55,7 +108,12 @@ const userController = {
   addBookToUserLibrary,
   getUserLibrary,
   deleteBookFromUserLibrary,
-  createUserList
+  addBookToUserList,
+  createUserList,
+  deleteUserListFromListId,
+  deleteBookFromListId,
+  changeListNameFromListId,
+  getUserList
 };
 
 export default userController;
