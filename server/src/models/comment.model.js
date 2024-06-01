d6edab807cf228f4a54f6f3193e1f8593a661fc7
Felipe Castillo <felipecastillo48@gmail.com>
2024-06-01 11:17:03 -0400

    chore comment and user model change
    comment: commentText property renamed to text   user: new property (description)

--- server/src/models/comment.model.js
+++ server/src/models/comment.model.js
@@ -3,7 +3,7 @@ import mongoose, { Types } from "mongoose";
 const CommentSchema = new mongoose.Schema({
   reviewId: { type: Types.ObjectId, required: true, ref: "reviews" },
   createdBy: { type: Types.ObjectId, required: true, ref: "users" },
-  commentText: { type: String, required: true },
+  text: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
