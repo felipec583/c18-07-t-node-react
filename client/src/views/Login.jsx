9072c87af34d74ad5e5e21705ae7e97aa7fd3aaa
pablo lopez <pablo_98_18@hotmail.com>
2024-06-08 00:27:31 -0300

    google login & baseUrl axios

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -3,6 +3,8 @@ import Navbar from "../components/Navbar";
 import FormLogin from "../components/FormLogin";
 import { FcGoogle } from "react-icons/fc";
 import { useRutes } from "../hooks/useRutes";
+import GoogleLoginButton from "../components/GoogleLoginButton";
+
 export default function Login() {
   const { goToRegister } = useRutes();
   return (
@@ -24,11 +26,7 @@ export default function Login() {
             </div>
             <FormLogin />
             <div className="w-full h-[0px] border-2 border-zinc-400"></div>
-            <div className="w-full h-[40px] my-5 px-7 py-4 bg-white rounded-lg border border-zinc-500 justify-start items-center gap-6 inline-flex">
-              <button className="flex items-center justify-center text-zinc-600 text-xl font-normal font-['Inter']">
-                <FcGoogle className="mx-4 w-5 h-5" /> Continuar con Google
-              </button>
-            </div>
+            <GoogleLoginButton />
             <div className="flex">
               <div className="text-neutral-700 text-base font-normal font-['Inter']">
                 ¿No tenes una cuenta en Bookpal?
